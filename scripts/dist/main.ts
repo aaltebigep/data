cc4963cfc4024279dfaca9659d7a9c8dd59143cb
Sena <contact@sena.pink>
2025-11-12 19:53:38 +0300

    fix(scripts/dist): doğru düzgün temizle

--- scripts/dist/main.ts
+++ scripts/dist/main.ts
@@ -1,7 +1,10 @@
 import { join } from "@std/path";
 import { DIST_DIR, indexEntities } from "./common.ts";
+import { exists } from "@std/fs";
 
-await Deno.remove(DIST_DIR, { recursive: true });
+if (await exists(DIST_DIR)) {
+  await Deno.remove(DIST_DIR, { recursive: true });
+}
 
 const data = {
   "members": await indexEntities("members/"),
